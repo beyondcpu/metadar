7584d432c57e6d66e866475a27a91462fc7da872
cugopal <cugopal@a9ee331d-12c6-4d92-188e-103603675f1e>
2012-05-24 13:57:57 +0000

    example for random forest model validation added

--- examples/modelvalidation.R
+++ examples/modelvalidation.R
@@ -3,9 +3,15 @@ source("~/Dropbox/Work/METADAR/Dataset.R")
 library("ROCR")
 source("~/Dropbox/Work/METADAR/ModelValidationCV.R")
 source("~/Dropbox/Work/METADAR/LogisticRegressionModelValidationCV.R")
+source("~/Dropbox/Work/METADAR/RandomForestModelValidationCV.R")
 
 ### If you have only training data
 dat <- new("Dataset", "~/Dropbox/Work/varp/Sample_DiscTraining_alvs.csv")
 lrmv <- new("LogisticRegressionModelValidationCV", dat, "Class", cv=100, selectedVariables=c("6", "7", "8"))
 lrmv$buildCV()
 lrmv$plotROC("example")
+
+library("randomForest")
+rfmv <- new("RandomForestModelValidationCV", dat, "Class", dat, "Class", cv=100)
+rfmv$buildCV2()
+rfmv$plotROC("example")
