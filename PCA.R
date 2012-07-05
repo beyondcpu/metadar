498592716e9ca53bd42561728602dc0ae7c060f9
cugopal <cugopal@a9ee331d-12c6-4d92-188e-103603675f1e>
2012-07-05 08:21:49 +0000

    Many changes, especially, a little bit of reorganization of functions & scripts

---
+++ PCA.R
@@ -0,0 +1,77 @@
+setGeneric("pca", function(Object, annotation, color, scale, title) standardGeneric("pca"))
+
+setMethod("pca", signature=c("Dataset", "character", "character", "logical", "missing"),
+    function(Object, annotation, color, scale) {
+      expr <- exprs(Object)
+      if(scale) {
+        exprs <- t(scale(t(expr)))
+      }
+      pr <- prcomp(expr, center=FALSE, scale=FALSE)
+      prop.var <- signif((pr$sdev^2 / sum(pr$sdev^2)) * 100, 2)
+      plot(pr$rotation[,c(1,2)], xlab=paste("PC1:", prop.var[1], "% variance"),
+           ylab=paste("PC2:", prop.var[2], "% variance"),
+           type="n", main="Principal component analysis")
+      annotation.colors <- as.factor(pData(Object)[,color])
+      levels(annotation.colors) <- 1:length(levels(annotation.colors))
+      text(pr$rotation[,c(1,2)], as.character(pData(Object)[,annotation]),
+           col=as.character(annotation.colors))
+      pr
+    })
+
+setMethod("pca", signature=c("Dataset", "character", "character", "logical", "character"),
+    function(Object, annotation, color, scale, title) {
+      expr <- exprs(Object)
+      if(scale) {
+        exprs <- t(scale(t(expr)))
+      }
+      pr <- prcomp(expr, center=FALSE, scale=FALSE)
+      prop.var <- signif((pr$sdev^2 / sum(pr$sdev^2)) * 100, 2)
+      plot(pr$rotation[,c(1,2)], xlab=paste("PC1:", prop.var[1], "% variance"),
+           ylab=paste("PC2:", prop.var[2], "% variance"),
+           type="n", main=title)
+      annotation.colors <- as.factor(pData(Object)[,color])
+      levels(annotation.colors) <- 1:length(levels(annotation.colors))
+      text(pr$rotation[,c(1,2)], as.character(pData(Object)[,annotation]),
+           col=as.character(annotation.colors))
+      pr
+    })
+
+setMethod("pca", signature=c("Dataset", "character", "character", "missing", "missing"),
+    function(Object, annotation, color) {
+      ### assume scaling if it is not provided
+      scale = TRUE
+      expr <- exprs(Object)
+      if(scale) {
+        exprs <- t(scale(t(expr)))
+      }
+      pr <- prcomp(expr, center=FALSE, scale=FALSE)
+      prop.var <- signif((pr$sdev^2 / sum(pr$sdev^2)) * 100, 2)
+      plot(pr$rotation[,c(1,2)], xlab=paste("PC1:", prop.var[1], "% variance"),
+           ylab=paste("PC2:", prop.var[2], "% variance"),
+           type="n", main="Principal component analysis")
+      annotation.colors <- as.factor(pData(Object)[,color])
+      levels(annotation.colors) <- 1:length(levels(annotation.colors))
+      text(pr$rotation[,c(1,2)], as.character(pData(Object)[,annotation]),
+           col=as.character(annotation.colors))
+      pr
+    })
+
+setMethod("pca", signature=c("Dataset", "character", "character", "missing", "character"),
+    function(Object, annotation, color, title) {
+      ### assume scaling if it is not provided
+      scale = TRUE
+      expr <- exprs(Object)
+      if(scale) {
+        exprs <- t(scale(t(expr)))
+      }
+      pr <- prcomp(expr, center=FALSE, scale=FALSE)
+      prop.var <- signif((pr$sdev^2 / sum(pr$sdev^2)) * 100, 2)
+      plot(pr$rotation[,c(1,2)], xlab=paste("PC1:", prop.var[1], "% variance"),
+           ylab=paste("PC2:", prop.var[2], "% variance"),
+           type="n", main=title)
+      annotation.colors <- as.factor(pData(Object)[,color])
+      levels(annotation.colors) <- 1:length(levels(annotation.colors))
+      text(pr$rotation[,c(1,2)], as.character(pData(Object)[,annotation]),
+           col=as.character(annotation.colors))
+      pr
+    })
---
+++ colhclust.R
@@ -0,0 +1,49 @@
+setGeneric("colhclust", function(Object, labels, color) standardGeneric("colhclust"))
+
+setMethod("colhclust", signature=c("Dataset", "character", "character"),
+    function(Object, labels, color) {
+      expr <- t(exprs(Object))
+      rownames(expr) <- pData(Object)[,labels]
+      
+      dd <- hclust(dist(expr))
+      local({
+        colLab <<- function(n) {
+          if(is.leaf(n)) {
+            a <- attributes(n)
+            i <<- i+1
+            attr(n, "nodePar") <-
+              c(a$nodePar, list(lab.col = mycols[i]))
+          }
+          n
+        }
+        mycols <- as.factor(pData(Object)[dd$order,color])
+        levels(mycols) <- 1:length(levels(mycols))
+        mycols <- as.character(mycols)
+        i <- 0
+      })
+      dL <- dendrapply(as.dendrogram(dd), colLab)
+      dL
+    })
+
+setMethod("colhclust", signature=c("Dataset", "missing", "character"),
+          function(Object, color) {
+            expr <- t(exprs(Object))            
+            dd <- hclust(dist(expr))
+            local({
+              colLab <<- function(n) {
+                if(is.leaf(n)) {
+                  a <- attributes(n)
+                  i <<- i+1
+                  attr(n, "nodePar") <-
+                    c(a$nodePar, list(lab.col = mycols[i]))
+                }
+                n
+              }
+              mycols <- as.factor(pData(Object)[dd$order,color])
+              levels(mycols) <- 1:length(levels(mycols))
+              mycols <- as.character(mycols)
+              i <- 0
+            })
+            dL <- dendrapply(as.dendrogram(dd), colLab)
+            dL
+          })
