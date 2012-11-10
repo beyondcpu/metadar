155a733475544defacc67acff49eba945fcb3766
cugopal <cugopal@a9ee331d-12c6-4d92-188e-103603675f1e>
2012-11-10 10:54:25 +0000

    Function for beanplots added Bug fix in Dataset (odds ratio function)

--- beanPlot.R
+++ beanPlot.R
@@ -1,19 +1,20 @@
-setGeneric("boxPlot", function(Object, covariate1, covariate2) standardGeneric("boxPlot"))
+setGeneric("beanPlot", function(Object, covariate1, covariate2) standardGeneric("beanPlot"))
 
-setMethod("boxPlot", signature=c("Dataset", "character", "missing"),
+setMethod("beanPlot", signature=c("Dataset", "character", "missing"),
           function(Object, covariate1) {
             for(i in seq(nrow(Object))) {
               fit <- aov(x~f, data=data.frame("x"=exprs(Object)[i,],
                                               "f"=factor(pData(Object)[,covariate1])
               ))
-              boxplot(exprs(Object)[i,]~factor(pData(Object)[,covariate1]),
+              beanplot(exprs(Object)[i,]~factor(pData(Object)[,covariate1]),
+                       col = c("#7FCCBB", "#004C3B", "#7FCCBB"),
                       main=paste("Name:", featureNames(Object)[i],
                                  "\nAnova P-value:",
                                  signif(summary(fit)[[1]]["f","Pr(>F)"], 2)))
             }
           })
 
-setMethod("boxPlot", signature=c("Dataset", "character", "character"),
+setMethod("beanPlot", signature=c("Dataset", "character", "character"),
           function(Object, covariate1, covariate2) {
             previous <- par(mai=c(0.5,0.5,1,0.2))
             for(i in seq(nrow(Object))) {
@@ -26,8 +27,9 @@ setMethod("boxPlot", signature=c("Dataset", "character", "character"),
               p2 <- signif(summary(fit)[[1]][2,"Pr(>F)"], 2)
               p3 <- signif(summary(fit)[[1]][3,"Pr(>F)"], 2)
               
-              boxplot(exprs(Object)[i,]~factor(paste(pData(Object)[,covariate1],
+              beanplot(exprs(Object)[i,]~factor(paste(pData(Object)[,covariate1],
                                                      pData(Object)[,covariate2])),
+                       col = c("#7FCCBB", "#004C3B", "#7FCCBB"),
                       main=paste("Name:", featureNames(Object)[i],
                                  "\n", covariate1, "p-value", p1,
                                  "\n", covariate2, "p-value", p2,
